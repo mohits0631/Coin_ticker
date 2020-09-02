aa2ea2451a2347bdff4f1785a8260b65c6eb1552
mohit singh <mohit.s0631@gmail.com>
2020-09-03 04:54:03 +0530

    Add files via upload

---
+++ lib/coin_data.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const List<String> currenciesList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptoList = ['BTC', 'ETH', 'LTC'];
+
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = 'E2302C8F-50AA-4C98-944E-DC123C3CD05D';
+
+class CoinData {
+  Future getCoinData(String selectedCurrency) async {
+    //TOD  4: Use a for loop here to loop through the cryptoList and request the data for each of them in turn.
+    Map<String, String> cryptoPrices = {};
+
+    for (String cryptoCurrency in cryptoList) {
+      String requestURL =
+          '$coinAPIURL/$cryptoCurrency/$selectedCurrency?apikey=$apiKey';
+      http.Response response = await http.get(requestURL);
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        double lastPrice = decodedData['rate'];
+        cryptoPrices[cryptoCurrency] = lastPrice.toStringAsFixed(0);
+      } else {
+        print(response.statusCode);
+        throw 'Problem with the get request';
+      }
+    }
+    return cryptoPrices;
+    //TOD 5: Return a Map of the results instead of a single value.
+  }
+}
